6a66a4d18014618dfcd598ff5ae327aa9102a2e5
Preveen Padmanabhan <preveen.padmanabhan@raptordesign.in>
2021-12-05 11:23:41 +0530

    - Added hello.sol

---
+++ hello.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+contract SolidityTest {
+	constructor public() {
+
+	}
+
+	function getResult() public view returns(unit) {
+		uint a=1;
+		uint b=2;
+
+		uint result = a + b;
+		return result;
+	}
+}
